a23faa6cddfee3963bf51392abf2160117c2b42a
Mizuho <mizuho128@gmail.com>
2026-08-31 20:22:35 +0900

    bin/claude.sh claude → bash -lic 'claude...'

--- bin/claude.sh
+++ bin/claude.sh
@@ -1,1 +1,5 @@
-ro-env $PWD /tmp $HOME/.cache $HOME/.claude{,.json,.json.lock} $HOME/.espressif -- claude --resume $1
+if [ -z "$1" ]; then
+  ro-env $PWD /tmp $HOME/.cache $HOME/.claude{,.json,.json.lock} $HOME/.espressif -- /usr/bin/bash -lic 'claude'
+else
+  ro-env $PWD /tmp $HOME/.cache $HOME/.claude{,.json,.json.lock} $HOME/.espressif -- /usr/bin/bash -lic "claude --resume $1"
+fi
